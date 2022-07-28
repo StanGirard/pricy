45e473796f3aab1b5879ddfe161f9f8c0651db52
StanGirard <stanislasg@padok.fr>
2022-07-28 13:31:55 +0200

    feat: 🎸 formating of dates new functions get start and end date

--- internal/format/dates.go
+++ internal/format/dates.go
@@ -1,6 +1,20 @@
 package format
 
+import "fmt"
+
 type DateInterval struct {
 	Start string `json:"start"`
 	End   string `json:"end"`
+}
+
+func (d DateInterval) GetStartDate() string {
+	return d.Start
+}
+
+func (d DateInterval) GetEndDate() string {
+	return d.End
+}
+
+func (d DateInterval) Print() {
+	fmt.Printf("Start: %s, End: %s\n", d.Start, d.End)
 }
