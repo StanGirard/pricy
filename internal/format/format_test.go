007505616d82853e0dd004c32afe4724c079543d
StanGirard <stanislasg@padok.fr>
2022-07-31 21:27:25 +0200

    test: 💍 added test on services and total formulas

--- internal/format/format_test.go
+++ internal/format/format_test.go
@@ -1,6 +1,8 @@
 package format
 
-var ServicePrices []Service = []Service{
+import "testing"
+
+var ServicePrices Services = []Service{
 	{
 		Name:  "Amazon Elastic Compute Cloud",
 		Units: "USD",
@@ -9,5 +11,46 @@ var ServicePrices []Service = []Service{
 			{Start: "2019-02-01", End: "2019-02-28"}: 0.0,
 			{Start: "2019-03-01", End: "2019-03-31"}: 0.0,
 		},
+		PriceEvolution: map[DateInterval]float64{
+			{Start: "2019-01-01", End: "2019-01-31"}: 0.0,
+			{Start: "2019-02-01", End: "2019-02-28"}: 0.02,
+			{Start: "2019-03-01", End: "2019-03-31"}: 0.03,
+		},
 	},
+}
+
+func TestServicesPrices(t *testing.T) {
+	if len(ServicePrices) != 1 {
+		t.Errorf("Expected 1 service, got %d", len(ServicePrices))
+	}
+	for _, service := range ServicePrices {
+		if len(service.DatePrice) != 3 {
+			t.Errorf("Expected 3 dates, got %d", len(service.DatePrice))
+		}
+		if len(service.PriceEvolution) != 3 {
+			t.Errorf("Expected 3 dates, got %d", len(service.PriceEvolution))
+		}
+		if service.DatePrice[DateInterval{Start: "2019-01-01", End: "2019-01-31"}] != 0.0 {
+			t.Errorf("Expected 0.0, got %f", service.DatePrice[DateInterval{Start: "2019-01-01", End: "2019-01-31"}])
+		}
+		if service.PriceEvolution[DateInterval{Start: "2019-01-01", End: "2019-01-31"}] != 0.0 {
+			t.Errorf("Expected 0.0, got %f", service.PriceEvolution[DateInterval{Start: "2019-01-01", End: "2019-01-31"}])
+		}
+	}
+}
+
+func TestTypeService(t *testing.T) {
+	var service Service
+	if service.Name != "" {
+		t.Errorf("Expected empty string, got %s", service.Name)
+	}
+	if service.Units != "" {
+		t.Errorf("Expected empty string, got %s", service.Units)
+	}
+	if len(service.DatePrice) != 0 {
+		t.Errorf("Expected empty map, got %d", len(service.DatePrice))
+	}
+	if len(service.PriceEvolution) != 0 {
+		t.Errorf("Expected empty map, got %d", len(service.PriceEvolution))
+	}
 }
--- internal/format/total_test.go
+++ internal/format/total_test.go
@@ -1,1 +1,41 @@
 package format
+
+import "testing"
+
+func TestTotalCostUsage(t *testing.T) {
+	var service Service
+	service.Name = "Amazon Elastic Compute Cloud"
+	service.Units = "USD"
+	service.DatePrice = map[DateInterval]float64{
+		{Start: "2019-01-01", End: "2019-01-31"}: 0.1,
+		{Start: "2019-02-01", End: "2019-02-28"}: 0.1,
+		{Start: "2019-03-01", End: "2019-03-31"}: 0.1,
+	}
+	service.PriceEvolution = map[DateInterval]float64{
+		{Start: "2019-01-01", End: "2019-01-31"}: 0.0,
+		{Start: "2019-02-01", End: "2019-02-28"}: 0.02,
+		{Start: "2019-03-01", End: "2019-03-31"}: 0.03,
+	}
+
+	var service2 Service
+	service2.Name = "Amazon Elastic Compute Cloud 2"
+	service2.Units = "USD"
+	service2.DatePrice = map[DateInterval]float64{
+		{Start: "2019-01-01", End: "2019-01-31"}: 0.1,
+		{Start: "2019-02-01", End: "2019-02-28"}: 0.1,
+		{Start: "2019-03-01", End: "2019-03-31"}: 0.1,
+	}
+	service2.PriceEvolution = map[DateInterval]float64{
+		{Start: "2019-01-01", End: "2019-01-31"}: 0.0,
+		{Start: "2019-02-01", End: "2019-02-28"}: 0.02,
+		{Start: "2019-03-01", End: "2019-03-31"}: 0.03,
+	}
+
+	services := []Service{service, service2}
+	totalCost := TotalCostUsage(services)
+	for _, totalPerDay := range totalCost {
+		if totalPerDay.TotalCost != 0.2 {
+			t.Errorf("Expected 0.1, got %f", totalPerDay.TotalCost)
+		}
+	}
+}
